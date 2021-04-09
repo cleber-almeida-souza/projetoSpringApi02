3e116d688dc818aa45a924475d736381378c325c
cleber-almeida-souza <cleberalsouza@gmail.com>
2021-04-08 22:10:19 -0300

    Modificações nos serviços da API e Repositório

--- src/main/java/br/com/cotiinformatica/controllers/LoginController.java
+++ src/main/java/br/com/cotiinformatica/controllers/LoginController.java
@@ -12,54 +12,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import br.com.cotiinformatica.dtos.UsuarioCadastroDTO;
-import br.com.cotiinformatica.entities.Usuario;
+import br.com.cotiinformatica.dtos.UsuarioLoginDTO;
 import br.com.cotiinformatica.repositories.UsuarioRepository;
-import br.com.cotiinformatica.validations.UsuarioCadastroValidation;
- 
-@Controller
-public class UsuarioController {
+import br.com.cotiinformatica.validations.UsuarioLoginValidation;
 
+@Controller
+public class LoginController {
+	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	// declarando o ENDPOINT dos serviços de usuario
-	private static final String ENDPOINT = "/api/usuarios";
+	private static final String ENDPOINT = "/api/login";
 
 	@RequestMapping(value = ENDPOINT, method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<List<String>> cadastrarUsuario(@RequestBody UsuarioCadastroDTO dto) {
-		
+	public ResponseEntity<List<String>> autenticarUsuario(@RequestBody UsuarioLoginDTO dto) {
+
 		List<String> mensagens = new ArrayList<String>();
-		
+
 		try {
 			
-			//aplicando as validações..
-			mensagens = UsuarioCadastroValidation.validate(dto);
+			//aplicar as validações no DTO..
+			mensagens = UsuarioLoginValidation.validate(dto);
 			
 			//verificando se não ocorreram erros de validação..
 			if(mensagens.size() == 0) {
 				
-				//verificar se o email informado já está cadastrado..
-				if(usuarioRepository.findByEmail(dto.getEmail()) != null) {					
-					throw new Exception("O email informado já encontra-se cadastrado.");
+				//verificar se o usuario existe no banco de dados (email e senha)
+				if(usuarioRepository.findByEmailSenha(dto.getEmail(), dto.getSenha()) != null) {
+					
+					//TODO GERAR TOKEN
+					
+					mensagens.add("Usuário autenticado com sucesso.");
+					
+					//retornar status de sucesso 200 (OK)
+					return ResponseEntity
+							.status(HttpStatus.OK)
+							.body(mensagens);		
 				}
-				
-				//realizar o cadastro do usuário
-				Usuario usuario = new Usuario();
-				
-				usuario.setNome(dto.getNome());
-				usuario.setEmail(dto.getEmail());
-				usuario.setSenha(dto.getSenha());
-				
-				usuarioRepository.save(usuario);
-				
-				mensagens.add("Usuário cadastrado com sucesso.");
-				
-				//retornar status de sucesso 201 (CREATED)
-				return ResponseEntity
-						.status(HttpStatus.CREATED)
-						.body(mensagens);				
+				else {
+					throw new Exception("Acesso negado. Usuário inválido.");
+				}		
 			}
 			else { //se houverem erros de validação
 				
@@ -68,19 +62,29 @@ public class UsuarioController {
 						.status(HttpStatus.BAD_REQUEST)
 						.body(mensagens);				
 			}
+			
 		}
 		catch(Exception e) {
 			
-			mensagens.add("Ocorreu um erro: " + e.getMessage());
+			mensagens.add("Erro: " + e.getMessage());
 			
 			//retornar status de erro 500 (INTERNAL SERVER ERROR)
 			return ResponseEntity
 					.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(mensagens);			
+					.body(mensagens);	
 		}
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
